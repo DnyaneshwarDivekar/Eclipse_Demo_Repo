d1ad88a5fa07324bcba548695ab72bc8a2e221ca
DnyaneshwarDivekar <dnyaneshwardivekar@gmail.com>
2022-05-28 16:25:59 +0530

    This is 3rd Commit

--- LWD_001/src/test/java/com/lwd/testpackage/TestClass_001.java
+++ LWD_001/src/test/java/com/lwd/testpackage/TestClass_001.java
@@ -10,6 +10,8 @@ public class TestClass_001 {
 		System.out.println("sum of a and b is :" + (a+b));
 		
 		System.out.println("sub of a and b is ::" + (a-b));
+		
+		System.out.println("mul of a and b is :: " + a*b );
 	}
 
 }
